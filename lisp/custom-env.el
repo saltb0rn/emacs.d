d7f0864151b6ebd76a1a64ca9595c45bcb7f260d
saltb0rn <asche34@outlook.com>
2017-10-01 00:02:59 +0800

    select new packages

--- lisp/custom-env.el
+++ lisp/custom-env.el
@@ -46,7 +46,7 @@
  '(inhibit-startup-screen nil)
  '(package-selected-packages
    (quote
-    (typing-game speed-type slime bison-mode emojify xref-js2 web-mode use-package tabbar rainbow-delimiters org-plus-contrib nyan-mode monokai-theme magit js2-refactor indium highlight-indent-guides geiser flycheck-ycmd fic-mode evil emms elpy ecb company-tern coffee-mode all-the-icons ace-window)))
+    (company-jedi typing-game speed-type slime bison-mode emojify xref-js2 web-mode use-package tabbar rainbow-delimiters org-plus-contrib nyan-mode monokai-theme magit js2-refactor indium highlight-indent-guides geiser flycheck-ycmd fic-mode evil emms elpy ecb company-tern coffee-mode all-the-icons ace-window)))
  '(python-shell-interpreter "python3")
  '(slime-auto-select-connection (quote always))
  '(slime-auto-start (quote always))
