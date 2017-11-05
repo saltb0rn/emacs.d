5046ce7df44fa83d01323976e580e58f49738ff1
saltb0rn <asche34@outlook.com>
2017-11-05 22:49:48 +0800

    update ignore rules

--- lisp/init-google-translate.el
+++
@@ -1,6 +0,0 @@
-(require-install 'google-translate)
-(require 'google-translate-default-ui)
-(global-set-key "\C-c t" 'google-translate-at-point)
-(global-set-key "\C-c T" 'google-translate-query-translate)
-
-hello
--- lisp/init-site-lisp.el
+++
@@ -1,5 +0,0 @@
-;;; Initialize action to load site-lisp
-(add-subdirs-to-load-path
- (expand-file-name "site-lisp/" user-emacs-directory))
-
-(provide 'init-site-lisp)
