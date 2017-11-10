7f5bc9d2586a829b7cf40ff30c2bb5b0cf5b8b64
saltb0rn <asche34@outlook.com>
2017-11-10 23:09:48 +0800

    New customizable variable that is the additional arguments for ssh method of tramp-mode

--- lisp/custom-env.el
+++ lisp/custom-env.el
@@ -9,6 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(additional-ssh-login-args (quote (("-o" "ServerAliveInterval=60"))))
  '(auto-save-default nil)
  '(blink-cursor-mode nil)
  '(coffee-indent-like-python-mode t)
--- mypkgs/sysconf-1.0/sysconf-tramp.el
+++ mypkgs/sysconf-1.0/sysconf-tramp.el
@@ -1,15 +1,22 @@
 (require 'tramp)
 
-(tramp-set-completion-function
- "ssh"
- `((tramp-parse-sconfig
-    ,(join-path
-      (file-name-directory load-file-name)
-      "dotfiles"
-      "ssh"
-      "config"))
-   (tramp-parse-sconfg "/etc/ssh_config")
-   (tramp-parse-sconfig "~/.ssh/config")))
+(defgroup tramp-conf nil
+  "Configurations that are not provided for tramp-mode"
+  :group 'sysconf)
+
+(defcustom
+  additional-ssh-login-args
+  '(())
+  "Add additional args you want for ssh command.
+The value should be an alist which stands for the option list
+"
+  :safe 'consp
+  :group 'tramp-conf)
+
+(setf (cadr (assoc 'tramp-login-args (assoc "ssh" tramp-methods)))
+      (append
+       additional-ssh-login-args
+       (cadr (assoc 'tramp-login-args (assoc "ssh" tramp-methods)))))
 
 ;;; These functions does not work for me.
 
