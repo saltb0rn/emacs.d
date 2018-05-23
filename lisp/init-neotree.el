b5f122351482bd345364bf67bd8d77f879a9339a
Saltb0rn <asche34@outlook.com>
2018-05-23 17:25:52 +0800

    Delete old files

--- lisp/init-neotree.el
+++
@@ -1,8 +0,0 @@
-(require-install 'neotree)
-
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-
-(global-set-key [f8] 'neotree-toggle)
-(global-set-key [f9] 'neotree-enter)
-
-(provide 'init-neotree)
--- lisp/init-term.el
+++
@@ -1,7 +0,0 @@
-(require-install 'multi-term)
-
-(custom-set-variables
- '(term-default-bg-color "#000000")
- '(term-default-fg-color "#ddd000"))
-
-(provide 'init-term)
