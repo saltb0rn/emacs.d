b5c5f0f8d526424ec30d629712962504cb437139
saltb0rn <asche34@outlook.com>
2019-03-06 16:15:23 +0800

    update: configuation for history API

--- assets/webpack/webpack.config.js
+++ assets/webpack/webpack.config.js
@@ -69,6 +69,9 @@ var devServer = {
     compress: true,
     inline: true,
     index: "/html/index.html",
+    historyApiFallback: {
+        index: "/html/index.html",
+    },
     hot: true,
 };
 
