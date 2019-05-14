95efbfce7e0e145750303aa62ea5d51b5b82e7e9
saltb0rn <asche34@outlook.com>
2019-05-14 13:29:05 +0800

    Enable interaction-log-mode

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
 ;; NOTE: about how to use bash on Windows, maybe I can try this thread: https://www.reddit.com/r/emacs/comments/4z8gpe/using_bash_on_windows_for_mx_shell/
 (defcustom path-to-bash-on-Windows nil "Set the path to bash while on Windows")
 
+;; NOTE: this one will improve performance while on Windows compared to CMD
 (when (memq system-type '(windows-nt ms-dos cygwim))
   (when path-to-bash-on-Windows
     (setq shell-file-name (concat
@@ -111,7 +112,6 @@
 ;; revert buffers automatically when underlying files are changed externally
 (global-auto-revert-mode t)
 
-
 ;; auto highlight left parenthesis and right parenthesis while cursor at position of either of them
 (show-paren-mode 1)
 
@@ -138,14 +138,11 @@
 ;; smart tab behavior - (or indent complete)
 (setq tab-always-indent 'complete)
 
-
-
 ;; Load additional files for reading
 (and load-file-name
      (let ((load-extras-path (concat (file-name-directory load-file-name) "load-extras.el")))
        (unless (file-exists-p load-extras-path) (write-region "" "" load-extras-path))
        (load load-extras-path)))
-
 
 ;; Restart Emacs
 ;; https://emacs.stackexchange.com/questions/5428/restart-emacs-from-within-emacs/5446#5446?newreg=d67eacb8fb3849b28a1cd89ac1769461
@@ -1057,7 +1054,6 @@ After creating the new empty project, go to the example/example and execute \"np
            (delete-directory webpack-project-root t))
          (prin1 exn))))))
 
-
 (use-package company-tern
   :unless (memq system-type '(windows-nt ms-dos cygwin))
   :ensure t
@@ -1094,7 +1090,7 @@ After creating the new empty project, go to the example/example and execute \"np
   :ensure t)
 
 (use-package interaction-log
-  :unless (memq system-type '(windows-nt ms-dos cygwin))
+  ;; :unless (memq system-type '(windows-nt ms-dos cygwin))
   :ensure t
   :config
   (interaction-log-mode +1)
