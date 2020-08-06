6bfeed23261ca66fc95156a96fa0a3c1edc4efe9
saltb0rn <asche34@outlook.com>
2020-08-06 19:55:07 +0800

    Update

--- site-lisp/exact/exact.el
+++ site-lisp/exact/exact.el
@@ -4,9 +4,8 @@
 
 ;;; Code:
 
-(setq-local lexical-binding t)
-
-(require 'cl-lib)
+(eval-and-compile
+  (require 'cl-lib))
 
 (cl-defstruct
     (exact-fraction
@@ -15,48 +14,72 @@
                    (numerator &optional (denominator 1) (ef-gcd 1))))
   numerator denominator ef-gcd)
 
-(defsubst exact-number-to-fraction (num)
-  (unless (numberp num)
-    (error "exact.el: Argument must be a number")))
+(defun exact-float-to-fraction (float &optional precision)
+  "https://stackoverflow.com/questions/95727/how-to-convert-floats-to-human-readable-fractions"
+  (let* ((precision (or precision 1000000000))
+         (floating-part (- float (floor float)))
+         (ef-gcd (gcd (round (* floating-part precision)) precision))
+         (denominator (/ precision ef-gcd))
+         (numerator (/ (round (* floating-part precision)) ef-gcd)))
+    (list numerator denominator)))
 
-(defun exact-fraction-create (numerator denominator)
-  (let* ((ef-gcd (exact-gcd numerator denominator))
+(defsubst exact-fraction--from-number (num)
+  (unless (numberp num)
+    (error "exact-number-to-fraction: Argument must be a number"))
+  (cond ((integerp num) (exact-fraction--create num 1))
+        ((floatp num) (exact-float-to-fraction num))
+        (nil ((error "exact-number-to-fraction: Argument must be a number")))))
+
+(defsubst exact-fraction--simplify (num)
+  "TODO: Deals with floating-point number."
+  (if (exact-fraction-p num)
+      (let* ((n
+              (exact-fraction--simplify (exact-fraction-numerator num)))
+             (d
+              (exact-fraction--simplify (exact-fraction-denominator num))))
+        (exact-fraction--create
+         (* (exact-fraction-numerator n) (exact-fraction-denominator d))
+         (* (exact-fraction-numerator d) (exact-fraction-denominator n))))
+    (exact-fraction--from-number num)))
+
+(defun exact-fraction-normalize (num)
+  (let* ((snum (exact-fraction--simplify num))
+         (n (exact-fraction-numerator snum))
+         (d (exact-fraction-denominator snum))
+         (ef-gcd (gcd n d)))
+    (exact-fraction--create (/ n ef-gcd) (/ d ef-gcd) ef-gcd)))
+
+(exact-fraction-normalize (exact-fraction--create 10 (exact-fraction--create 3 (exact-fraction--create 2 5))))
+
+(defun exact-fraction-create (numerator &optional denominator)
+  (let* ((denominator (or denominator 1))
+         (ef-gcd (gcd numerator denominator))
          (ef-numerator (/ numerator ef-gcd))
          (ef-denominator (/ denominator ef-gcd)))
     (exact-fraction--create ef-numerator ef-denominator ef-gcd)))
 
-
 ;; (defmacro exact-add (&rest args)
-;;   (let ((tlcm (make-symbol "ef-lcm"))
-;;         (nf (make-symbol "numerator-factorys"))
-;;         (argsed (make-symbol "argsed")))
-;;     `(let ((,argsed (list ,@args)))
-;;        (let ((,tlcm (exact-lcm-lst (mapcar #'exact-fraction-denominator ,argsed)))
-;;              (,nf (apply #'mapcar (lambda (x) (/ ,tlcm x)) (mapcar #'exact-fraction-numerator ,argsed))))
-;;          ,nf
-;;          )
-;;        )
-;;     ))
+;;   (let ((e-args (make-symbol "evaluated-args"))
+;;         (ns (make-symbol "numerators"))
+;;         (ds (make-symbol "denominator"))
+;;         (nfs (make-symbol "nfs"))
+;;         (ef-lcm (make-symbol "ef-lcm")))
+;;     `(let* ((,e-args (list ,@args))
+;;             (,ns (mapcar #'exact-fraction-numerator ,e-args))
+;;             (,ds (mapcar #'exact-fraction-denominator ,e-args))
+;;             (,ef-lcm (apply #'lcm ,ds))
+;;             (,nfs
+;;              (mapcar* (lambda (x y) (* (/ ,ef-lcm x) y)) ,ds ,ns)))
+;;        (exact-fraction-create (reduce #'+ ,nfs) ,ef-lcm))))
+;; (pp-macroexpand-expression
+;;  '(exact-add (exact-fraction-create 10) (exact-fraction-create 3 4) (exact-fraction-create 7 4)))
 
-
-;; (macroexpand '(exact-add (exact-fraction-create 10 2) (exact-fraction-create 2 3)))
-
-;; (exact-add (exact-fraction-create 10 2) (exact-fraction-create 2 3))
-
-
-(defmacro exact-add (&rest args)
-  (let ((eargs (make-symbol "eargs"))
-        (ns (make-symbol "ns"))
-        (ds (make-symbol "ds"))
-        (nfs (make-symbol "nfs"))
-        (ef-lcm (make-symbol "ef-lcm")))
-    `(let* ((,eargs (list ,@args))
-            (,ns (mapcar #'exact-fraction-numerator ,eargs))
-            (,ds (mapcar #'exact-fraction-denominator ,eargs))
-            (,ef-lcm (apply #'lcm ,ds))
-            (,nfs
-             (mapcar* (lambda (x y) (* (/ ,ef-lcm x) y)) ,ds ,ns)))
-       (exact-fraction-create (reduce #'+ ,nfs) ,ef-lcm))))
+(defun exact-add (&rest args)
+  (let* ((numerators (mapcar #'exact-fraction-numerator args))
+         (denominators (mapcar #'exact-fraction-denominator args))
+         (ef-lcm (apply #'lcm denominators))
+         (nfs (mapcar* (lambda (x y) (* (/ ef-lcm x) y)) denominators numerators)))
+    (exact-fraction-create (reduce #'+ nfs) ef-lcm)))
 
 (provide 'exact)
 
