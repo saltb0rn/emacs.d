a0471bbf7bd38c652b7b047a7c19dc655bc1ccad
saltb0rn <asche34@outlook.com>
2020-11-26 20:01:04 +0800

    Update

--- init.el
+++ init.el
@@ -168,7 +168,10 @@
 (set-default 'indent-tabs-mode nil)
 
 (prefer-coding-system 'utf-8)
+
 (set-default-coding-systems 'utf-8)
+(set-language-environment 'utf-8)
+(setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 
--- site-lisp/pokemon/pokemon-dex-dat.el
+++ site-lisp/pokemon/pokemon-dex-dat.el
@@ -1,87 +1,69 @@
 (require 'pokemon-utils)
 
+(defun pokemon--get-library-path ()
+  (file-name-directory (locate-library "pokemon")))
+
 (defcustom pokemon-pokedex-rby-dat
-  (expand-file-name "assets/pokedex-rby.json")
+  (concat (pokemon--get-library-path) "assets/pokedex-rby.json")
   "Path to pokedex data file of Pokemon Red, Blue & Yellow (Gen 1)"
   :type 'string)
 
 (defcustom pokemon-pokedex-gsc-dat
-  (expand-file-name "assets/pokedex-gsc.json")
+  (concat (pokemon--get-library-path) "assets/pokedex-gsc.json")
   "Path to pokedex data file of Pokemon Gold, Silver & Crystal (Gen 2)"
   :type 'string)
 
 (defcustom pokemon-pokedex-rse-dat
-  (expand-file-name "assets/pokedex-rse.json")
+  (concat (pokemon--get-library-path) "assets/pokedex-rse.json")
   "Path to pokedex data file of Pokemon Ruby, Sapphire & Emerald (Gen 3)"
   :type 'string)
 
 (defcustom pokemon-pokedex-dpp-dat
-  (expand-file-name "assets/pokedex-dpp.json")
+  (concat (pokemon--get-library-path) "assets/pokedex-dpp.json")
   "Path to pokedex data file of Pokemon Diamond, Pearl & Platinum (Gen 4)"
   :type 'string)
 
 (defcustom pokemon-pokedex-bw-dat
-  (expand-file-name "assets/pokedex-bw.json")
+  (concat (pokemon--get-library-path) "assets/pokedex-bw.json")
   "Path to pokedex data file of Pokemon Black & White (Gen 5)"
   :type 'string)
 
 (defcustom pokemon-pokedex-xy-dat
-  (expand-file-name "assets/pokedex-xy.json")
+  (concat (pokemon--get-library-path) "assets/pokedex-xy.json")
   "Path to pokedex data file of Pokemon X & Y (Gen 6)"
   :type 'string)
 
 (defcustom pokemon-pokedex-sm-dat
-  (expand-file-name "assets/pokedex-sm.json")
+  (concat (pokemon--get-library-path) "assets/pokedex-sm.json")
   "Path to pokedex data file of Pokemon Sun & Month (Gen 7)"
   :type 'string)
 
 (defcustom pokemon-pokedex-ss-dat
-  (expand-file-name "assets/pokedex-ss.json")
+  (concat (pokemon--get-library-path) "assets/pokedex-ss.json")
   "Path to pokedex data file of Pokemon Sword & Shield (Gen 8)"
   :type 'string)
 
-;; Pokemon Ruby
-(defun pokemon-dex-rby-update ()
-  (pokemon-load-data-from-json pokemon-pokedex-rby-dat))
-
-(defun pokemon-dex-gsc-update ()
-  (pokemon-load-data-from-json pokemon-pokedex-gsc-dat))
-
-(defun pokemon-dex-rse-update ()
-  (pokemon-load-data-from-json pokemon-pokedex-rse-dat))
-
-(defun pokemon-dex-dpp-update ()
-  (pokemon-load-data-from-json pokemon-pokedex-dpp-dat))
-
-(defun pokemon-dex-bw-update ()
-  (pokemon-load-data-from-json pokemon-pokedex-bw-dat))
-
-(defun pokemon-dex-xy-update ()
-  (pokemon-load-data-from-json pokemon-pokedex-xy-dat))
-
-(defun pokemon-dex-sm-update ()
-  (pokemon-load-data-from-json pokemon-pokedex-sm-dat))
-
-(defun pokemon-dex-ss-update ()
-  (pokemon-load-data-from-json pokemon-pokedex-ss-dat))
+(defmacro pokemon-dex-gen-update (gen)
+  `(pokemon-load-data-from-json
+    (symbol-value (intern (format "pokemon-pokedex-%s-dat" (symbol-name (quote ,gen)))))))
 
 (defun pokemon-dex-rby ()
-  (pokemon-dex-rby-update))
+  (pokemon-dex-gen-update rby))
 
 (defun pokemon-dex-gsc ()
   (pokemon-data-update
-   (pokemon-dex-rby-update)
-   (pokemon-dex-gsc-update)))
+   (pokemon-dex-rby)
+   (pokemon-dex-gen-update gsc)))
 
 (defun pokemon-dex-rse ()
   (pokemon-data-update
    (pokemon-dex-gsc)
-   (pokemon-dex-rse-update)))
+   (pokemon-dex-gen-update rse)))
 
 (defun pokemon-dex-dpp ()
   (let ((dex (pokemon-data-update
               (pokemon-dex-rse)
-              (pokemon-dex-dpp-update)))
+              (pokemon-dex-gen-update dpp)))
         (pokemons-to-delete-abs
          (list
           "Machop"
@@ -123,13 +105,13 @@
 (defun pokemon-dex-bw ()
   (pokemon-data-update
    (pokemon-dex-dpp)
-   (pokemon-dex-bw-update)))
+   (pokemon-dex-gen-update bw)))
 
 (defun pokemon-dex-xy ()
   (let ((dex
          (pokemon-data-update
           (pokemon-dex-bw)
-          (pokemon-dex-xy-update)))
+          (pokemon-dex-gen-update xy)))
         (pokemons-to-delete-abs
          (list
           "Duskull"
@@ -149,12 +131,12 @@
 (defun pokemon-dex-sm ()
   (pokemon-data-update
    (pokemon-dex-xy)
-   (pokemon-dex-sm-update)))
+   (pokemon-dex-gen-update sm)))
 
 (defun pokemon-dex-ss ()
   (let ((dex (pokemon-data-update
               (pokemon-dex-sm)
-              (pokemon-dex-ss-update)))
+              (pokemon-dex-gen-update ss)))
         (pokemons-to-delete-abs (list "Gengar" "Raikou" "Entei" "Suicune")))
     (dolist (pokemon pokemons-to-delete-abs)
       (pokemon-data-rem "ab" (pokemon-data-get dex pokemon)))
