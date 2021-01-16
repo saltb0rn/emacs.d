29272eaf367fdb7b7d5cb89d9eb9d8491cea9701
saltb0rn <asche34@outlook.com>
2021-01-16 10:40:43 +0800

    Fixed the problem while on termux

--- init.el
+++ init.el
@@ -142,7 +142,10 @@
   (tool-bar-mode -1))
 
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
+
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
 (blink-cursor-mode 0)
 (display-battery-mode 1)
 
