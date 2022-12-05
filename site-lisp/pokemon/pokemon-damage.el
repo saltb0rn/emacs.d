9368c0ae556d329c2d77f09d74ca163d38bfeb6e
Saltb0rn <asche34@outlook.com>
2022-12-05 17:25:12 +0800

    修改伤害函数为宏

--- site-lisp/pokemon/pokemon-damage.el
+++ site-lisp/pokemon/pokemon-damage.el
@@ -2,12 +2,12 @@
 
 ;; https://www.trainertower.com/dawoblefets-damage-dissertation/
 
-(defun pokemon-base-damage (atker-lvl atk/spa def/spd mov-dmg)
+(defun pokemon-base-damage (atker-lvl atk/spa def/spd mov-power)
   (let* ((step1
           (pokemon-flooring
            (*
             (pokemon-flooring (+ (* 2 atker-lvl) 10) 5)
-            mov-dmg
+            mov-power
             atk/spa)
            def/spd))
          (step2
@@ -43,15 +43,15 @@
      protect-mod)
   (let* ((step-spread-mov-mod ;; spread mov mod
           (pokemon-round (* base-damage (/ spread-mov-mod pokemon-mod-denominator))))
-         
+
          (step-parental-bond-mod ;; only applied the second hit of the move
           (pokemon-round
            (* step-spread-mov-mod (/ parental-bond-mod pokemon-mod-denominator))))
-         
+
          (step-weather-mod ;; weather mod
           (pokemon-round
            (* step-parental-bond-mod  (/ weather-mod pokemon-mod-denominator))))
-         
+
          (step-critical-hit-mod (pokemon-round (* step-weather-mod critical-mod)))
 
          ;; from now on, the calc result will be a list
@@ -63,71 +63,83 @@
                (pokemon-flooring (* step-critical-hit-mod (- 100 random-factor)) 100) res)
               (setq random-factor (1+ random-factor)))
             res))
-         
+
          (step-same-type-atk-bouns-mod ;; same type mod
           (mapcar
            (lambda (dmg)
              (pokemon-round (* dmg (/ same-type-atk-bouns-mod pokemon-mod-denominator))))
            step-random-mod))
-         
+
          (step-type-eff-mod ;; type eff mod
           (mapcar
            (lambda (dmg)
-             ;; 值应该是 2^n, 如果有一个克制, 那么 n = 1, 如果有一个被克制那么就是 n = -1, 一个单纯有效就是 n = 0
+             ;; 值应该是 2^n, 如果有一个克制, 那么 n = 1
+             ;; 如果有一个被克制那么就是 n = -1
+             ;; 一个单纯有效就是 n = 0
              (pokemon-flooring (* dmg type-eff-mod)))
            step-same-type-atk-bouns-mod))
-         
+
          (step-burn-mod (mapcar
                           (lambda (dmg)
                             (pokemon-round
                              (* dmg (/ burn-mod pokemon-mod-denominator))))
                           step-type-eff-mod))
-         
+
          (step-final-mods ;; final mods, wrong
-          (let ((mod (pokemon--chain-mods final-mods)))
+          (let ((mod (pokemon--chain-mods
+                      (cons pokemon-mod-denominator final-mods))))
             (mapcar
              (lambda (dmg)
                (pokemon-round (* dmg (/ mod pokemon-mod-denominator))))
              step-burn-mod)))
-         
+
+         (step-protect-mod ;; 保护被Z招式或者极巨化穿透时
+          (mapcar
+           (lambda (dmg)
+             (pokemon-round (* dmg (/ protect-mod pokemon-mod-denominator))))
+           step-final-mods))
+
          (step-one-damage-check  ;; one damage check
           (mapcar
            (lambda (v) (or (and (= 0 v) 1) v))
-           step-final-mods)))
-    step-one-damage-check))
+           step-protect-mod))
 
-(defun pokemon-damage-calc (
-     atker-lvl
-     atk/spa                            ;; 攻击 mods, 比如战斗种能力的升降, 以及讲究头带, 水珠等道具影响
-     def/spd                            ;; 防御 mods, 和攻击 mods 类似
-     mov-dmg                            ;; 招式伤害也有 mods, 比如地形, 帮手
-     ;; general mods
-     &optional
-     spread-mov-mod
-     parental-bond-mod
-     weather-mod
-     critical-mod
-     same-type-atk-bouns-mod
-     type-eff-mod
-     burn-mod
-     final-mods
-     protect-mod)
-  (let ((base-damage
-         (pokemon-base-damage
-          atker-lvl
-          atk/spa
-          def/spd
-          mov-dmg)))
-    (pokemon--damage-calc
-     base-damage
-     (or spread-mov-mod pokemon-mod-denominator)
-     (or parental-bond-mod pokemon-mod-denominator)
-     (or weather-mod pokemon-mod-denominator)
-     (or critical-mod 1)
-     (or same-type-atk-bouns-mod pokemon-mod-denominator)
-     (or type-eff-mod 1)
-     (or burn-mod pokemon-mod-denominator)
-     (or final-mods nil)
-     protect-mod)))
+         (step-ffff-damage-check  ;; 65535 damage check
+          (mapcar
+           (lambda (v) (or (and (< v #xffff) v) (% v #xffff)))
+           step-one-damage-check))
+         )
+    step-ffff-damage-check))
+
+(defun pokemon--damage-calc-mod-check (plst)
+  (pcase plst
+    (`nil t)
+    (`(,key ,value) t)
+    (`(,key ,value . ,rest)
+     (pokemon-damage-calc--mod-check rest))))
+
+(defmacro pokemon-damage-calc (atker-lvl atk/spa def/spd mov-power &rest mods)
+  `(if (pokemon--damage-calc-mod-check ',mods)
+     (pokemon--damage-calc
+      ,(pokemon-base-damage
+        atker-lvl
+        atk/spa                         ;; 受到攻击 mods 影响, 包括能力升降低, 特性加成, 道具影响
+        def/spd                         ;; 同攻击 mods
+        mov-power)                      ;; 受到场地和天气的影响, 有些特性也会影响, 比如妖精皮肤在同类加成 1.2 倍, 招式也会有影响, 比如帮助加成 2 倍
+      ,(or (plist-get mods :spread-mov-mod) pokemon-mod-denominator)
+      ,(or (plist-get mods :parental-bond-mod) pokemon-mod-denominator)
+      ,(or (plist-get mods :weather-mod) pokemon-mod-denominator)
+      ,(or (plist-get mods :critical-mod) 1)
+      ,(or (plist-get mods :same-type-atk-bouns-mod) pokemon-mod-denominator)
+      ,(or (plist-get mods :type-eff-mod) 1)
+      ,(or (plist-get mods :burn-mod) pokemon-mod-denominator)
+      ,(or (plist-get mods :final-mods) nil)
+      ,(or (plist-get mods :protect-mod) pokemon-mod-denominator))
+     0))
+
+;; examples:
+;; (pokemon-damage-calc 50 232 109 120
+;;                       :same-type-atk-bouns-mod pokemon-mod-x1dot5
+;;                       :final-mods '(4096 2732 2048 3072 5324))
 
 (provide 'pokemon-damage)
